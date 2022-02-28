70883569faaa95e839f292ea9c9aa0813b2be1b8
waqarakbar03 <waqar.akbar@hotmail.com>
2022-02-28 16:50:19 +0000

    week 4 exercise 3 html and js file with paragraph and heading

--- wk-4/ex-3.js
+++ wk-4/ex-3.js
@@ -1,5 +1,7 @@
 function myFunction() {
-    var element = document.getElementById('page_heading');
-    element.firstChild.nodeValue = 'New heading';
+    var element1 = document.getElementById('page_heading');
+    element1.firstChild.nodeValue = 'New heading';
+    var element2 = document.getElementById('paragraph');
+    element2.firstChild.nodeValue = 'New paragraph';
 }
 document.addEventListener('DOMContentLoaded', myFunction);
