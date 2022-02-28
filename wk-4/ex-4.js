984e55158c0ca678894e211ef81c305a8ebfeba4
waqarakbar03 <waqar.akbar@hotmail.com>
2022-02-28 16:57:04 +0000

    week 4 js function with click

---
+++ wk-4/ex-4.js
@@ -0,0 +1,7 @@
+function myFunction() {
+    var element1 = document.getElementById('page_heading');
+    element1.firstChild.nodeValue = 'New heading';
+    var element2 = document.getElementById('paragraph');
+    element2.firstChild.nodeValue = 'New paragraph';
+}
+document.addEventListener('click', myFunction);
