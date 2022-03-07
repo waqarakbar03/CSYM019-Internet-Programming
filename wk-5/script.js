25888bef26a79e3ce0b84d30f2f35f6b8a892b58
waqarakbar03 <waqar.akbar@hotmail.com>
2022-03-07 16:29:11 +0000

    exercise 5 with opacity incriment

--- wk-5/script.js
+++ wk-5/script.js
@@ -1,10 +1,11 @@
 function myFuction() {
     var element = document.getElementById('circle');
-    element.style.backgroundColor = 'blue';
-    element.style.opacity = '1';
+    var circleOpacity = parseFloat(element.style.opacity)
+    element.style.opacity = circleOpacity + 0.1;
 }
 function myLoadFuction() {
     var element = document.getElementById('circle');
+    element.style.opacity = '0.5'
     element.addEventListener('click', myFuction);
 }
 document.addEventListener('DOMContentLoaded', myLoadFuction);
