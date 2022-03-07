97975abd366e312e386b1f9c5c4acd33734172a3
waqarakbar03 <waqar.akbar@hotmail.com>
2022-03-07 15:47:08 +0000

    ex-2 changed color opacity to 1

--- wk-5/script.js
+++ wk-5/script.js
@@ -1,6 +1,7 @@
 function myFuction() {
     var element = document.getElementById('circle');
     element.style.backgroundColor = 'blue';
+    element.style.opacity = '1';
 }
 function myLoadFuction() {
     var element = document.getElementById('circle');
