d857a43b40e4aa2980b4652793cdcc0770a0f0a6
waqarakbar03 <waqar.akbar@hotmail.com>
2022-03-07 16:37:47 +0000

    add timer to decrease the opacity

--- wk-5/script.js
+++ wk-5/script.js
@@ -1,11 +1,12 @@
 function myFuction() {
     var element = document.getElementById('circle');
     var circleOpacity = parseFloat(element.style.opacity)
-    element.style.opacity = circleOpacity + 0.1;
+    element.style.opacity = circleOpacity - 0.01;
+    setInterval(myFuction, 1000);
 }
 function myLoadFuction() {
     var element = document.getElementById('circle');
-    element.style.opacity = '0.5'
+    element.style.opacity = '1'
     element.addEventListener('click', myFuction);
 }
 document.addEventListener('DOMContentLoaded', myLoadFuction);
